be2b6789e2a071b4a023d501b40386ad2d85739a
Manuel <koe18463@spengergasse.at>
2018-11-15 18:18:58 +0100

    login.php -> Login finished && Register almost done

--- website-dir/login.php
+++ website-dir/login.php
@@ -61,7 +61,8 @@
                             $data = mysqli_fetch_assoc($query);
                             $_SESSION['user_id'] = $data["u_id"];
                             $_SESSION['username'] = $data["u_username"];
-                            echo "<h1 class='display-4'>Hallo,<br> " . $_SESSION["username"] . ".</h1>";
+                            $_SESSION['forename'] = $data["u_forename"];
+                            header("Location: home.php");
                         } else {
                             echo "<h4 class='mb-3 text-danger'>Something went wrong. Please log in again!</h4>";
                             exit(file_get_contents('html/login.html'));
@@ -72,13 +73,46 @@
                     }
                     break;
                 case "reg":
-                    if (!empty($_POST["username"]) && !empty($_POST["email"]) && !empty($_POST["pwd"]) && !empty($_POST["forename"]) && !empty($_POST["surname"]) && !empty("zipcode") && !empty($_POST["phonenr"])) {
-                        if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+                    try {
+                        // TODO field birthday instead of phonenr, because birthday NotNull field in DB
+                        if (!empty($_POST["username"]) && !empty($_POST["email"]) && !empty($_POST["pwd"]) && !empty($_POST["forename"]) && !empty($_POST["surname"]) && !empty("zipcode") && !empty($_POST["phonenr"])) {
+                            $username = mysqli_real_escape_string($_POST["username"]);
+                            $queryUsername = mysqli_query($conn, "SELECT * FROM u_users WHERE u_username = '$username';");
+                            if (mysqli_num_rows($queryUsername) > 0) {
+                                throw new Exception("User already exists");
+                            }
+                            if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+                                $email = mysqli_real_escape_string($conn, $_POST["email"]);
+                            } else {
+                                throw new Exception("Invalid email!");
+                            }
+                            $pwd = hash("sha384", $_POST["pwd"], FALSE);
+                            $forename = mysqli_real_escape_string($conn, $_POST["forename"]);
+                            $surename = mysqli_real_escape_string($conn, $_POST["surname"]);
+                            if (strlen($_POST["zipcode"]) < 11) {
+                                $zipcode = mysqli_real_escape_string($conn, $_POST["zipcode"]);
+                            } else {
+                                throw new Exception("Invalid zipcode, pls use between 0 to 10 chars!");
+                            }
+                            // TODO concat all birthday fields(yyyy-MM-dd) and check if valid -> insert the right way into DB
+                            // INSERT INTO u_users (u_birthdate) VALUE (STR_TO_DATE($birthdate['20010501'], '%Y%m%d'));
+                            $birthdate = null;
 
+                            mysqli_query($conn, "INSERT INTO u_users (u_username, u_email, u_pwd, u_surname, u_forename, u_birthdate, u_zipcode) VALUES ('$username','$email','$pwd','$surename','$forename','$birthdate','$zipcode');");
+                            if (mysqli_affected_rows() == 1) {
+                                $_SESSION['user_id'] = null;
+                                $_SESSION['username'] = $username;
+                                $_SESSION['forename'] = $forename;
+                                header("Location: home.php");
+                            } else {
+                                throw new Exception("DB: Insert Failure");
+                            }
                         } else {
-
+                            throw new Exception("At least one required field was empty");
                         }
-                        $pwd = hash("sha384", $_POST["pwd"], FALSE);
+                    } catch (Exception $e) {
+                        echo "<h4 class='mb-3 text-danger'>".$e->getMessage()." Please register again!</h4>";
+                        exit(file_get_contents('html/login.html'));
                     }
                     break;
                 default:
@@ -90,6 +124,7 @@
             die("Connection failed: " . mysqli_connect_error());
         }
     } else {
+        header("Location: index.php");
     }
     ?>
 </section>
