eef5514d1b80dd142b1fc02fcb450b5e28fb25e6
Manuel <koe18463@spengergasse.at>
2018-11-15 14:56:32 +0100

    xx

---
+++ website-dir/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Manuel
+ * Date: 15.11.2018
+ * Time: 12:00
+ */
+const DB_USER = "root";
+const DB_PWD = "";
+const DB_HOST = "127.0.0.1";
+const DB_NAME = "";
