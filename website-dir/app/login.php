0a927b049565296599125498dfaf13755c14c113
Florian Parfuss <par18293@spengergasse.at>
2019-01-13 13:25:05 +0100

    login endlich fertig :))))))

--- website-dir/app/login.php
+++ website-dir/app/login.php
@@ -40,6 +40,9 @@ session_start();
                 echo $e->getMessage();
             }
             if ($conn) {
+                foreach ($_POST as $id => $value) {
+                    echo $value;
+                }
                 switch ($_POST["submit"]) {
                     case "login":
                         $username = mysqli_real_escape_string($conn, $_POST["username"]);
@@ -63,17 +66,8 @@ session_start();
                             $forename = mysqli_real_escape_string($conn, $_POST["forename"]);
                             $surname = mysqli_real_escape_string($conn, $_POST["surname"]);
                             $phonenr = mysqli_real_escape_string($conn, $_POST["phonenr"]);
-                            if(strlen($pwd) > 8){
-                                $pwd = hash("sha384", $_POST["pwd"], FALSE);
-                            }
-                            else{
-                                throw new Exception("Bitte ein Passwort unter 8 Zeichen benutzen");
-                            }
-                            if (strlen($_POST["zipcode"]) < 11 && strlen($_POST[""]) > 4) {
-                                $zipcode = mysqli_real_escape_string($conn, $_POST["zipcode"]);
-                            } else {
-                                throw new Exception("Falsche Postleitzahl, Bitte eine zwischen 4 und 10 Zeichen nehmen!");
-                            }
+                            $pwd = hash("sha384", $_POST["pwd"], FALSE);
+                            $zipcode = mysqli_real_escape_string($conn, $_POST["zipcode"]);
                             ///Testet Ob Username vorhanden
                             if (isUserbyName($username) || isUserbyEmail($email)) {
                                 throw new Exception("Username or Email existiert bereits");
@@ -84,14 +78,13 @@ session_start();
                             $birthdate = null;
 
                             mysqli_query($conn, "INSERT INTO u_users (u_username, u_email, u_pwd, u_surname, u_forename, u_birthdate, u_zipcode, u_phonenumber) VALUES ('$username','$email','$pwd','$surname','$forename','$birthdate','$zipcode','$phonenr');");
-                            $query = mysqli_query($conn, "SELECT u_id FROM u_users WHERE u_username =' $username '");
+                            $query = mysqli_query($conn, "SELECT * FROM u_users WHERE u_username = '$username' AND u_pwd LIKE '$pwd';");
                             if (mysqli_num_rows($query) == 1) {
                                 $data = mysqli_fetch_assoc($query);
                                 $user = new user($data["u_id"], $data["u_username"], $data["u_forename"], $data["u_surname"], $data["u_email"], $data["u_birthdate"], $data["u_createtime"], $data["u_description"], $data["u_image"], $data["u_phonenumber"], $data["u_zipcode"]);
                                 $_SESSION["u_user"] = $user;
                                 header("Location: home.php");
                             }
-
                         } catch (Exception $e) {
                             echo "<h4 class='mb-3 text-danger'>" . $e->getMessage() . " Please register again!</h4>";
                             exit(file_get_contents('../html/login.html'));
