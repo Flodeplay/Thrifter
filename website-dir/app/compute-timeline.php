aa1c263dc8d6e2cb5bf03fd03e2205f535ec22ca
Florian Parfuss <par18293@spengergasse.at>
2019-01-14 17:35:30 +0100

    timeline dummy finished ... | compute-timeline

---
+++ website-dir/app/compute-timeline.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flori
+ * Date: 14.01.2019
+ * Time: 17:35
+ */
--- website-dir/app/timeline.php
+++ website-dir/app/timeline.php
@@ -42,7 +42,7 @@ checkSession();
 </head>
 <body>
 <?php
-echo file_get_contents('../html/navs.html');
+/*echo file_get_contents('../html/navs.html');*/
 echo file_get_contents('../html/bottommenu.html');
 ?>
 <header class="shadow">
@@ -63,8 +63,10 @@ echo file_get_contents('../html/bottommenu.html');
     </div>
     <section id="timeline" class="d-flex">
         <div class="timeline-product" style="background-color: white; border-bottom: 1px; border-color: #4e555b; max-width: 700px">
-            <div style="background-color: white; border-bottom: 1px solid black; margin: 0 5px"><a href='viewuser.php?username="franz"'><div class='card-body'><img style='max-height: 40px; width: auto' class='rounded-circle' src='../assets/users/Bild_Manu.jpg'><span class='pl-3'>Manuel</span></div></a></div>
+            <div style="background-color: white;margin: 0 5px"><a href='viewuser.php?username="franz"'><div class='card-body d-flex flex-row' style="font-size: 85%;"><img style='max-height: 40px; width: auto' class='rounded-circle' src='../assets/users/Bild_Manu.jpg'><div class="d-flex flex-column"><span class='pl-3'>Manuel</span><span class='pl-3'>1120</span></div></div></a></div>
             <img src="../assets/posts/1.png" style="width: 100%; height: auto; max-width: 700px">
+            <div style="background-color: white; margin: 10px 20px 0" class="d-flex justify-content-between align-items-center"><h3>Hoodie</h3><i class="fas fa-heart fa-2x text-success"></i></div>
+            <div style="background-color: white; margin: 0 10px;padding: 0  10px 10px; border-bottom: 1px solid black; " class="d-flex justify-content-between align-items-center"><h4>100€</h4><a><h4>Mehr</h4></a></div>
         </div>
     </section>
 </main>
